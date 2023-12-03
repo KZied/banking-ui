d536d7eabbbb458ef35a919e05dc42c4b466ecbd
Zied <zied.kannou15@gmail.com>
2023-12-03 18:25:32 +0100

    Adapt HTTP interceptor && update token retrieval

--- src/app/services/http-interceptor/http-interceptor.service.ts
+++ src/app/services/http-interceptor/http-interceptor.service.ts
@@ -10,15 +10,17 @@ export class HttpInterceptorService implements HttpInterceptor {
   constructor() { }
 
   intercept(req: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
-    const token = 'eyJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJ0ZXN0dGVzdEBnbWFpbC5jb20iLCJleHAiOjE3MDIzMzQ0NTcsImlhdCI6MTcwMTYxNDQ1NywiYXV0aG9yaXRpZXMiOlt7ImF1dGhvcml0eSI6IlJPTEVfVVNFUiJ9XX0.QFgX6EtXXN6aiCcZHT6TmpiCZ6RR-aM_5dtxba8cXu8';
-    if (token !== undefined && token !== null){
-      // assign the token;
-      const authReq = req.clone({
-        headers: new HttpHeaders({
-          Authorization: 'Bearer ' + token
-        })
-      });
-      return  next.handle(authReq);
+    if (!req.url.includes('/authenticate') || !req.url.includes('/register')){
+      const token = localStorage.getItem('token ');
+      if (token){
+        // assign the token;
+        const authReq = req.clone({
+          headers: new HttpHeaders({
+            Authorization: 'Bearer ' + token
+          })
+        });
+        return  next.handle(authReq);
+      }
     }
     return next.handle(req);
   }
