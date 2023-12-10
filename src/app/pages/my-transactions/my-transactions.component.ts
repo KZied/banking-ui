b8d473bd20f3e63ac89e0a7617dcdb5852589664
Zied <zied.kannou15@gmail.com>
2023-12-10 14:02:34 +0100

    My transactions page implementation

--- src/app/pages/my-transactions/my-transactions.component.ts
+++ src/app/pages/my-transactions/my-transactions.component.ts
@@ -1,10 +1,31 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {TransactionService} from "../../services/services/transaction.service";
+import {TransactionDto} from "../../services/models/transaction-dto";
+import {HelperService} from "../../services/helper/helper.service";
 
 @Component({
   selector: 'app-my-transactions',
   templateUrl: './my-transactions.component.html',
   styleUrls: ['./my-transactions.component.scss']
 })
-export class MyTransactionsComponent {
+export class MyTransactionsComponent implements OnInit {
+
+  transactions: Array<TransactionDto> = [];
+
+  constructor(
+    private transactionService: TransactionService,
+    private helperService: HelperService
+  ) {
+  }
+
+  ngOnInit(): void {
+    this.transactionService.findByUserId({
+      'user-id': this.helperService.userId
+    }).subscribe({
+      next: (data) => {
+        this.transactions = data;
+      }
+    });
+  }
 
 }
