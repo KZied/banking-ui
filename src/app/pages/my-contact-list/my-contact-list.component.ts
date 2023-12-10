19bdf2e43fe336ba00f3e9bba0351bb9340347fb
Zied <zied.kannou15@gmail.com>
2023-12-10 16:12:48 +0100

    My contact list implementation

--- src/app/pages/my-contact-list/my-contact-list.component.ts
+++ src/app/pages/my-contact-list/my-contact-list.component.ts
@@ -1,10 +1,31 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {ContactService} from "../../services/services/contact.service";
+import {ContactDto} from "../../services/models/contact-dto";
+import {HelperService} from "../../services/helper/helper.service";
 
 @Component({
   selector: 'app-my-contact-list',
   templateUrl: './my-contact-list.component.html',
   styleUrls: ['./my-contact-list.component.scss']
 })
-export class MyContactListComponent {
+export class MyContactListComponent implements OnInit {
+
+  contacts: Array<ContactDto> = [];
+
+  constructor(
+    private contactService: ContactService,
+    private helperService: HelperService
+  ) {
+  }
+
+  ngOnInit(): void {
+    this.contactService.findByUserId1({
+      'user-id': this.helperService.userId
+    }).subscribe({
+      next: (data) => {
+        this.contacts = data;
+      }
+    })
+  }
 
 }
