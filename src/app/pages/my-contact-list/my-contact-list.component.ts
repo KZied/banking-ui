c74eef78cae5277190df9b5205c58fbcce296797
Zied <zied.kannou15@gmail.com>
2023-12-17 21:50:14 +0100

    Delete contact implementation

--- src/app/pages/my-contact-list/my-contact-list.component.ts
+++ src/app/pages/my-contact-list/my-contact-list.component.ts
@@ -4,6 +4,7 @@ import {ContactDto} from "../../services/models/contact-dto";
 import {HelperService} from "../../services/helper/helper.service";
 import {Router} from "@angular/router";
 
+
 @Component({
   selector: 'app-my-contact-list',
   templateUrl: './my-contact-list.component.html',
@@ -12,15 +13,21 @@ import {Router} from "@angular/router";
 export class MyContactListComponent implements OnInit {
 
   contacts: Array<ContactDto> = [];
+  display = "none";
+  userIdToDelete: any = -1;
 
   constructor(
     private contactService: ContactService,
     private helperService: HelperService,
-    private router:Router
+    private router:Router,
   ) {
   }
 
   ngOnInit(): void {
+    this.findAllContactsByUser();
+  }
+
+  findAllContactsByUser(){
     this.contactService.findByUserId1({
       'user-id': this.helperService.userId
     }).subscribe({
@@ -32,5 +39,25 @@ export class MyContactListComponent implements OnInit {
 
   update(id: number | undefined) {
     this.router.navigate(['user/new-contact', id]);
+  }
+
+  openModal() {
+    this.display = "block";
+  }
+  closeModal() {
+    this.display = "none";
+  }
+
+  delete() {
+    if (this.userIdToDelete){
+      this.contactService.delete2({
+        'contact-id': this.userIdToDelete
+        }).subscribe({
+        next: () => {
+          this.closeModal();
+          this.findAllContactsByUser();
+        }
+      })
+    }
   }
 }
